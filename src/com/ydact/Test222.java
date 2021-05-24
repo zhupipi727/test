572c0215efc2a3a656a544d7b2bf14aeb04a39dc
caozhixx <caozhixxx@163.com>
2021-05-24 17:25:39 +0800

    second commit

---
+++ src/com/ydact/Test222.java
@@ -0,0 +1,10 @@
+package com.ydact;
+
+import java.util.Arrays;
+
+public class Test222 {
+    public static void main(String[] args) {
+        int[] a ={1,2,33};
+        System.out.println(Arrays.stream(a).sum());
+    }
+}
