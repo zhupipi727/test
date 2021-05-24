14aa4009cc1dc922aeaad69c6508a2f29fb8227f
caozhixx <caozhixxx@163.com>
2021-05-24 17:33:21 +0800

    hot fix second commit

--- src/com/ydact/Test222.java
+++ src/com/ydact/Test222.java
@@ -7,5 +7,6 @@ public class Test222 {
         int[] a ={1,2,33};
         System.out.println(Arrays.stream(a).sum());
         System.out.println("hotfx修改");
+        System.out.println("hotfx修改2");
     }
 }
