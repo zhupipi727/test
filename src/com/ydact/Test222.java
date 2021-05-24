a6a0197615639fb5079a1f6280fb6389cc52726d
caozhixx <caozhixxx@163.com>
2021-05-24 17:32:00 +0800

    hot fix first commit

--- src/com/ydact/Test222.java
+++ src/com/ydact/Test222.java
@@ -6,5 +6,6 @@ public class Test222 {
     public static void main(String[] args) {
         int[] a ={1,2,33};
         System.out.println(Arrays.stream(a).sum());
+        System.out.println("hotfx修改");
     }
 }
---
+++ src/com/ydact/Test_hotfix.java
@@ -0,0 +1,11 @@
+package com.ydact;
+
+import java.util.ArrayList;
+
+public class Test_hotfix {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 17;
+        System.out.println(a + b);
+    }
+}
